29a4dc245f8ba98798e18f3471d39f06d51666d1
mmartinslovik <martinslovik1122@gmail.com>
2022-07-04 12:53:40 +0200

    Refactor LINQs for better readability

--- Services/CharacterService.cs
+++ Services/CharacterService.cs
@@ -17,7 +17,7 @@ public class CharacterService : ICharacterService
     public async Task<ServiceResponse<List<GetCharacterDTO>>> GetCharacters() 
     {
         ServiceResponse<List<GetCharacterDTO>> serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
-        serviceResponse.Data = (characters.Select(character => _mapper.Map<GetCharacterDTO>(character))).ToList();
+        serviceResponse.Data = (characters.Select(c => _mapper.Map<GetCharacterDTO>(c))).ToList();
         
         return serviceResponse;
     }
@@ -25,7 +25,7 @@ public class CharacterService : ICharacterService
     public async Task<ServiceResponse<GetCharacterDTO>> GetCharacter(int id)
     {
         ServiceResponse<GetCharacterDTO> serviceResponse = new ServiceResponse<GetCharacterDTO>();
-        serviceResponse.Data = _mapper.Map<GetCharacterDTO>(characters.FirstOrDefault(character => character.Id == id));
+        serviceResponse.Data = _mapper.Map<GetCharacterDTO>(characters.FirstOrDefault(c => c.Id == id));
 
         return serviceResponse;
     }
@@ -34,9 +34,9 @@ public class CharacterService : ICharacterService
     {
         ServiceResponse<List<GetCharacterDTO>> serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
         Character character = _mapper.Map<Character>(newCharacter);
-        character.Id = characters.Max(character => character.Id) + 1;
+        character.Id = characters.Max(c => c.Id) + 1;
         characters.Add(character);
-        serviceResponse.Data = (characters.Select(character => _mapper.Map<GetCharacterDTO>(character))).ToList();
+        serviceResponse.Data = (characters.Select(c => _mapper.Map<GetCharacterDTO>(c))).ToList();
 
         return serviceResponse;
     }
