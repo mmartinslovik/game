70f78e287e7d185f4051a10a04b898044070c40e
mmartinslovik <martinslovik1122@gmail.com>
2022-07-04 10:52:04 +0200

    Implement GET by id and POST requests

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,17 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace dotnet_project1.Controllers;
 
 [ApiController]
 [Route("api/characters")]
-public class CharacterController : ControllerBase 
+public class CharacterController : ControllerBase
 {
     private static Character mage = new Character();
 
-    [HttpGet()]
-    public IActionResult Get() 
+    private static List<Character> characters = new List<Character>
     {
-        return Ok(mage);
+        new Character(),
+        new Character{ Id = 1, Name = "Khadgar" }
+    };
+
+    [HttpGet()]
+    public IActionResult Get()
+    {
+        return Ok(characters);
     }
 
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id) 
+    {
+        return Ok(characters.FirstOrDefault(character => character.Id == id));
+    }
+
+    [HttpPost]
+    public IActionResult AddCharacter(Character newCharacter)
+    {
+        characters.Add(newCharacter);
+        return Ok(characters);
+    }
 }
