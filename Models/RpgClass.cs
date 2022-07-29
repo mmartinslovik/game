0f60295532e32eb4553f9d9f1f461393b4f292fc
mmartinslovik <martinslovik1122@gmail.com>
2022-07-29 15:17:19 +0200

    Add new classes, refactor /login endpoint

--- Models/RpgClass.cs
+++ Models/RpgClass.cs
@@ -2,5 +2,11 @@ public enum RpgClass
     {
         Rogue = 1,
         Mage = 2,
-        Warrior = 3
+        Warrior = 3,
+        Paladin = 4,
+        Druid = 5,
+        Hunter = 6,
+        Warlock = 7,
+        Priest = 8,
+        Shaman = 9
     }
